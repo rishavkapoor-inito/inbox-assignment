2e3a13284b1f83d5b044c49c2067b1ce16c65eca
Rishav Kapoor <rishav.kapoor@inito.com>
2025-08-29 15:08:54 +0530

    workspace setup

---
+++ Inbox/Inbox/InboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  InboxApp.swift
+//  Inbox
+//
+//  Created by User on 29/08/25.
+//
+
+import SwiftUI
+
+@main
+struct InboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Inbox/InboxTests/InboxTests.swift
@@ -0,0 +1,17 @@
+//
+//  InboxTests.swift
+//  InboxTests
+//
+//  Created by User on 29/08/25.
+//
+
+import Testing
+@testable import Inbox
+
+struct InboxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
