1eb96a287bc55fa5f3ce16809a0ae17074198a9a
Rishav Kapoor <rishav.kapoor@inito.com>
2025-08-29 16:52:29 +0530

    Add external dependencies

--- Inbox/Inbox/InboxApp.swift
+++ Inbox/Inbox/InboxApp.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+
 @main
 struct InboxApp: App {
     var body: some Scene {
--- InboxNetworking/Package.swift
+++ InboxNetworking/Package.swift
@@ -4,21 +4,25 @@
 import PackageDescription
 
 let package = Package(
-    name: "InboxNetworking",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "InboxNetworking",
-            targets: ["InboxNetworking"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "InboxNetworking"),
-        .testTarget(
-            name: "InboxNetworkingTests",
-            dependencies: ["InboxNetworking"]
-        ),
-    ]
+  name: "InboxNetworking",
+  platforms: [.iOS(.v15)],
+  products: [.library(name: "InboxNetworking", targets: ["InboxNetworking"])],
+  dependencies: [
+    .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.10.0"),
+    .package(path: "../InboxResources")
+  ],
+  targets: [
+    .target(
+      name: "InboxNetworking",
+      dependencies: [
+        .product(name: "Alamofire", package: "Alamofire"),
+        "InboxResources"
+      ],
+      resources: []
+    ),
+    .testTarget(
+      name: "InboxNetworkingTests",
+      dependencies: ["InboxNetworking", "InboxResources"]
+    )
+  ]
 )
